b1d0dd67f3992968f1bd59acbae8806ca0a841a7
PauloChantal <paulochantal99@gmail.com>
2025-04-08 15:38:17 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,54 @@
 #include <stdio.h>
+#include <string.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// Definição da estrutura para a carta
+typedef struct {
+    char estado[3];       // Codigo do estado
+    char codigo[3];       // Codigo da carta
+    char nome[50];        // Nome da cidade
+    int populacao;        // Populacao
+    float area;           // Area em km
+    float pib;            // PIB em milhoes
+    int pontosTuristicos; // Numero de pontos turisticos
+} Carta;
+
+void exibirCarta(Carta c) {
+    printf("Carta Super Trunfo: %s\n", c.nome);
+    printf("Estado: %s\n", c.estado);
+    printf("Codigo da carta: %s\n", c.codigo);
+    printf("Populacao: %d habitantes\n", c.populacao);
+    printf("Area: %.2f km\n", c.area);
+    printf("PIB: R$ %.2f milhoes\n", c.pib);
+    printf("Numero de pontos turisticos: %d\n", c.pontosTuristicos);
+}
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    // Criando a carta de Teresina
+    Carta teresina;
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // Atribuindo os valores à carta
+    strcpy(teresina.estado, "P");           // Codigo do estado Piaui
+    strcpy(teresina.codigo, "P1");          // Codigo da carta
+    strcpy(teresina.nome, "Teresina");      // Nome da cidade
+    teresina.populacao = 926000;            // Populacao
+    teresina.area = 1723.4;                 // Area em km
+    teresina.pib = 17800000.00;             // PIB em milhoes
+    teresina.pontosTuristicos = 10;         // Numero de pontos turisticos
+
+
+    // Criando a carta de Sao Luis
+    Carta saoLuis;
+    strcpy(saoLuis.estado, "M");
+    strcpy(saoLuis.codigo, "M1");
+    strcpy(saoLuis.nome, "Sao Luis");
+    saoLuis.populacao = 1088057;
+    saoLuis.area = 834.00;
+    saoLuis.pib = 28000000.00;
+    saoLuis.pontosTuristicos = 11;
+
+    // Exibindo as cartas
+    exibirCarta(teresina);
+    exibirCarta(saoLuis);
 
     return 0;
 }
